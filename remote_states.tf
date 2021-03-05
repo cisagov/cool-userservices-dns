9d18646c9cb8ef1f0d35b880fc766571def5a550
David Redmin <david.redmin@trio.dhs.gov>
2021-03-04 23:10:11 -0500

    Add all necessary remote states

---
+++ remote_states.tf
@@ -0,0 +1,91 @@
+# ------------------------------------------------------------------------------
+# Retrieves state data from a Terraform backend. This allows use of the
+# root-level outputs of one or more Terraform configurations as input data
+# for this configuration.
+# ------------------------------------------------------------------------------
+
+data "terraform_remote_state" "dns_cyber_dhs_gov" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-dns-cyber.dhs.gov.tfstate"
+  }
+
+  # There is only one environment for the DNS account, so there is
+  # no need to match the current Terraform workspace.
+  workspace = "production"
+}
+
+data "terraform_remote_state" "master" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-accounts/master.tfstate"
+  }
+
+  # There is only one environment for this account, so there is
+  # no need to match the current Terraform workspace.
+  workspace = "production"
+}
+
+data "terraform_remote_state" "terraform" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-accounts/terraform.tfstate"
+  }
+
+  # There is only one environment for this account, so there is
+  # no need to match the current Terraform workspace.
+  workspace = "production"
+}
+
+data "terraform_remote_state" "users" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-accounts/users.tfstate"
+  }
+
+  # There is only one environment for this account, so there is
+  # no need to match the current Terraform workspace.
+  workspace = "production"
+}
+
+data "terraform_remote_state" "userservices" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-accounts-userservices/terraform.tfstate"
+  }
+
+  # The current Terraform workspace must match the name of one of the
+  # workspaces in cool-accounts-userservices (e.g. staging, production)
+  # or else this remote state will not work.
+  workspace = terraform.workspace
+}
